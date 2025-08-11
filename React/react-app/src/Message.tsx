da23d84bca3186953be8363b93c3e0960309ff3b
Mohit Dev Tomar <casonpark8@gmail.com>
2025-08-11 18:58:15 +0530

    Putting up the basic comp

---
+++ React/react-app/src/Message.tsx
@@ -0,0 +1,10 @@
+function Message(){
+    //JSX Js XML 
+    const name ='mohit';
+    if(name){
+        return <h1>hello {name}</h1>
+    }
+    return <h1>Hello world</h1>
+}
+
+export default Message;
